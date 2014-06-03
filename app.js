9366b4dc80d2333897a009fea96627b98789338b
Max Ogden <max@maxogden.com>
2014-06-03 12:47:50 -0700

    make it green

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var xhr = require('xhr')
 var fs = require('fs')
 var mustache = require('mustache').render
 var dom = require('domquery')
+var qs = require('querystring')
+
+var remote = 'http://localhost:6461'
+var state = {
+  offset: 0
+}
 
 window.dom = dom // for debugging
 
@@ -10,7 +16,50 @@ var templates = {
   generating: fs.readFileSync('./templates/generating.html').toString(),
   tableContainer: fs.readFileSync('./templates/tableContainer.html').toString(),
   actions: fs.readFileSync('./templates/actions.html').toString(),
-  dataTable: fs.readFileSync('./templates/dataTable.html').toString()
+  dataTable: fs.readFileSync('./templates/dataTable.html').toString(),
+  metadata: fs.readFileSync('./templates/metadata.html').toString()
+}
+
+fetchMetadata(function(err, metadata) {
+  if (err) return console.error(err)
+  render('metadata', '#metadata', metadata)
+  fetchAndRenderRows()
+})
+
+render('title', '.project-title', {db_name: 'Dat Database'})
+render('tableContainer', '.right-panel')
+activateControls()
+// render('generating', '.project-actions')
+// render('actions', '.project-actions', {db_name: 'Dat Database'} )
+
+function fetchMetadata(cb) {
+  xhr({ uri: remote + '/api', json: true }, function (err, resp, data) {
+    if (data) state.dbInfo = data
+    cb(err, data)
+  })
+}
+
+function fetchAndRenderRows(opts) {
+  if (!opts) {
+    state.offset = 0
+    opts = {}
+  }
+  
+  var query = {
+    start: opts.start,
+    tail: opts.tail,
+    limit: getPageSize()
+  }
+  
+  if (typeof query.tail !== 'undefined') state.offset = state.dbInfo.rows - query.limit
+  else if (typeof query.start !== 'undefined') state.offset = state.offset + query.limit
+  
+  var uri = remote + '/api/json?' + qs.stringify(query)
+  
+  xhr({ uri: uri, json: true }, function (err, resp, data) {
+    if (err) return console.error(err)
+    renderTable(data.rows)
+  })
 }
 
 function render(template, target, data) {
@@ -19,14 +68,8 @@ function render(template, target, data) {
   target.html(compiled)
 }
 
-var remote = 'http://localhost:6461'
-var opts = { uri: remote + '/api/json', json: true }
-
-xhr(opts, function (err, resp, data) {
-  var rows = data.rows
+function renderTable(rows) {
   if (rows.length === 0) return render('dataTable', '.data-table-container')
-  
-  dom('#docCount').text(rows.length + " documents")
   
   var headers = Object.keys(rows[0])
   var tableRows = []
@@ -49,9 +92,65 @@ xhr(opts, function (err, resp, data) {
     headers: headers,
     notEmpty: true
   })
-})
+  
+  dom('.viewpanel-pagingcount').text((state.offset + 1) + " - " + (state.offset + getPageSize()))
+  
+  state.newest = rows[0].key
+  state.oldest = rows[rows.length - 1].key
+  
+  if (state.offset + getPageSize() >= state.dbInfo.doc_count) {
+    deactivate(dom( '.viewpanel-paging .last'))
+    deactivate(dom( '.viewpanel-paging .next'))
+  } else {
+    activate(dom( '.viewpanel-paging .last'))
+    activate(dom( '.viewpanel-paging .next'))
+  }
 
-render('title', '.project-title', {db_name: 'Database'})
-render('generating', '.project-actions')
-render('tableContainer', '.right-panel')
-render('actions', '.project-actions', {db_name: 'Database'} )
+  if (state.offset === 0) {
+    deactivate(dom( '.viewpanel-paging .previous'))
+    deactivate(dom( '.viewpanel-paging .first'))
+  } else {
+    activate(dom( '.viewpanel-paging .previous'))
+    activate(dom( '.viewpanel-paging .first'))
+  }
+}
+
+function activate(e) {
+  e.removeClass('inaction').addClass('action')
+}
+
+function deactivate(e) {
+  e.removeClass('action').addClass('inaction')
+}
+
+function activateControls() {
+  dom('.viewPanel-pagingControls-page').on('click', function (event) {
+    dom(".viewpanel-pagesize .selected").removeClass('selected')
+    dom(event.target).addClass('selected')
+    fetchAndRenderRows(state.newest)
+  })
+  dom( '.viewpanel-paging a' ).on('click', function( e ) {
+    var action = dom(e.target)
+    if (action.hasClass("last")) {
+      fetchAndRenderRows({tail: getPageSize()})
+    }
+    if (action.hasClass("next")) {
+      fetchAndRenderRows({start: state.oldest})
+    }
+    if (action.hasClass("previous")) {
+      fetchAndRenderRows()
+    }
+    if (action.hasClass("first")) {
+      fetchAndRenderRows()
+    }
+  })
+}
+
+function getPageSize() {
+  var pagination = dom(".viewpanel-pagesize .selected")
+  if (pagination.length > 0) {
+    return parseInt(pagination.text())
+  } else {
+    return 10;
+  }
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ dat listen
 npm install
 npm start
 ```
+
+some CSS comes from the OpenRefine project (https://github.com/OpenRefine/OpenRefine/blob/master/LICENSE.txt)
